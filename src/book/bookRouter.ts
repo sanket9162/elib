1719447c6e06ad2baeb2a457abebc257ab7e4e16
sanket9162 <sanketgondhali55@gmail.com>
2024-05-11 21:11:13 +0530

    add list book endpoint

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBook, updateBook } from "./bookController";
+import { createBook, listBooks, updateBook } from "./bookController";
 import multer from "multer";
 import path from "node:path";
 import authenticate from "../middlewares/authenticate";
@@ -31,4 +31,5 @@ bookRouter.patch(
   updateBook
 );
 
+bookRouter.get("/", listBooks);
 export default bookRouter;
