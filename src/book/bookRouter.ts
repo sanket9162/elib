c73b2d56d510d93f68406270555b8b097fe05dc3
sanket9162 <sanketgondhali55@gmail.com>
2024-05-11 21:36:18 +0530

    add single book endpoint

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,10 @@
 import express from "express";
-import { createBook, listBooks, updateBook } from "./bookController";
+import {
+  createBook,
+  getSingleBook,
+  listBooks,
+  updateBook,
+} from "./bookController";
 import multer from "multer";
 import path from "node:path";
 import authenticate from "../middlewares/authenticate";
@@ -32,4 +37,6 @@ bookRouter.patch(
 );
 
 bookRouter.get("/", listBooks);
+bookRouter.get("/:bookId", getSingleBook);
+
 export default bookRouter;
